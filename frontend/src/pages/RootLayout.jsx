647a9417e29e202531712dcc3a10f8d6369205fa
Amit Manna <amitmannasm@gmail.com>
2025-01-30 20:46:57 +0530

    fixed ui for home page

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -7,9 +7,9 @@ import TopNav from '../components/widgets/TopNav'
 const RootLayout = () => {
     return (
           
-        <div className='w-screen h-screen'>
+        <div className='w-screen h-screen flex flex-col'>
             <TopNav />
-            <div className='w-full h-full'>
+            <div className='flex flex-1 w-full'>
             <Outlet  />
             </div>
         </div>
