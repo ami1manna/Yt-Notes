40c10bc5708bd3bc369039a9ac006468081f029f
Amit Manna <amitmannasm@gmail.com>
2025-02-03 17:05:27 +0530

    changed css for videoCArd

--- frontend/src/components/widgets/CourseList.jsx
+++ frontend/src/components/widgets/CourseList.jsx
@@ -10,14 +10,18 @@ const CourseList = () => {
     <>
       {console.log(userPlaylists)} {/* Debugging: Log the userPlaylists */}
       {userPlaylists.length === 0 && <FallBackScreen />} {/* Show fallback if no playlists */}
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-3">
-        {userPlaylists.map((playlist) => (
+      <div className=" h-max grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4 w-full auto-rows-auto">
+
+         {userPlaylists.map((playlist) => (
           <VideoCard
             key={playlist.playlistId} // Use playlistId as the unique key
             title={playlist.channelTitle || 'Untitled Playlist'} // Use channelTitle or a fallback
             thumbnailUrl={playlist.playlistThumbnailUrl} // Use playlistThumbnailUrl
           />
+
         ))}
+
+        
       </div>
     </>
   );
