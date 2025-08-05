b7b007ed867a2e1c1e079f563b97648e0bd7c3e6
ami1manna <amitmannasm@gmail.com>
2025-08-05 13:44:45 +0530

    added builde comd

--- socket-backend/server.js
+++ socket-backend/server.js
@@ -20,6 +20,7 @@ io.on("connection", (socket) => {
   registerSocketHandlers(io, socket);
 });
 
-server.listen(3000, () => {
-  console.log("🚀 Server listening on http://localhost:3000");
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
